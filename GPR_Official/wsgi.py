7a10390016f36146097b32ac9fc380a23f872449
BuleandraGeorge <buleandrageorge@gmail.com>
2020-12-18 11:44:56 +0000

    Initial Commit

---
+++ GPR_Official/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for GPR_Official project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GPR_Official.settings')
+
+application = get_wsgi_application()
