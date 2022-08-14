465419f7884240edf096aa21407755761665a8bf
George <buleandrageorge@gmail.com>
2022-08-14 11:23:10 +0100

    Translations 1

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Order
-
+from django.utils.translation import gettext_lazy as _
 class OrderForm(forms.ModelForm):
     class Meta():
         model = Order
@@ -11,4 +11,11 @@ class OrderForm(forms.ModelForm):
                     'adresa_linia_2',
                     'judet',
                     'tara',)
-
+        labels = {
+            'nume': _("Nume"),
+            'telefon':_('Telefon'),
+            'adresa':_('Adresa'),
+            'adresa_linia_2':_('Adresa linia 2'),
+            'judet':_('Judet'),
+            'tara':_('Tara')
+        }
